e6eab4a4a66db30942676f73506eb7c5df5c2e48
AvisRara <tsiganov.da@gmail.com>
2021-02-27 06:47:41 +0400

    промежуточный коммит

--- Task 1-2/js/main.js
+++ Task 1-2/js/main.js
@@ -1,15 +1,17 @@
 // 1. Переделайте makeGETRequest() так, чтобы она использовала промисы.
 // 2. Добавьте в соответствующие классы методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины.
 
-'use strict';
+// 'use strict';
 
-const url = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
-
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+class ParentList {
+    constructor() { }
+}
 // переделать в ДЗ на промисы. НЕ ИСПОЛЬЗОВАТЬ fetch!!!
-let getRequest = (url) => {
+let getRequest = (API) => {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
+        xhr.open('GET', API, true);
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
                 if (xhr.status !== 200) {
@@ -23,20 +25,89 @@ let getRequest = (url) => {
     })
 }
 
-class ProductItem {
-    constructor(id, title, price, img) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-        this.img = img;
+class Item {
+    constructor(el, linkImg = 'https://fakeimg.pl/200x200/282828/eae0d0/') {
+        this.id = el.id_product;
+        this.title = el.product_name;
+        this.price = el.price;
+        this.linkImg = linkImg;
     }
+}
+class CatalogItem extends Item {
     getHTML() {
         return `<div class="product-item">
                     <h3>${this.title}</h3>
-                    <img src= "${this.img}" alt="">
+                    <img src= "${this.linkImg}" alt="someImg">
                     <p>${this.price} &#8381;</p>
                     <button class="by-btn">Добавить в корзину</button>
                 </div>`;
+    }
+}
+
+class CartItem extends Item {
+    constructor(el) {
+        super(el);
+        this.quantity = el.quantity;
+    }
+    getHTML() {
+        return `<tr class="cart-item" data-id="${this.id}>
+                    <td>
+                        <img src="${this.linkImg}" alt="someImg" />
+                    </td>
+                    <td>${this.title}</td>
+                    <td>х ${this.quantity}</td>
+                    <td>${this.price} &#8381;</td>
+                    <td>
+                        <a href="#"><i class="bi bi-x-circle" data-id="${this.id}></i></a>
+                    </td>
+                </tr>`;
+    }
+}
+
+class Cart {
+    constructor(container = ".cart-container", url = 'getBasket.json') {
+        // super(el)
+        this.container = container;
+        this.listCart = [];
+        this.getData(url);
+        this.init();
+    }
+
+    getData(url) {
+        return fetch(`${API + url}`)
+            .then(response => response.json())
+            // .then(data => {
+            //     console.log('Данные от сервера\n', data)
+            //     return data;
+            // })
+            .catch(error => {
+                console.log('Ошибка получения данных');
+            })
+    }
+
+    getHTML(el) {
+
+    }
+
+    delItem(el) {
+        this.getData('deleteFromBasket.json')
+            .then(data => {
+                if (data.result === 1) {
+                    let dataId = +el.dataset['id'];
+                    document.querySelector(`.cart-item[data-id="${dataId}"]`).remove();
+                    console.log('удален товар с id =', dataId)
+                } else {
+                    console.log('ошибка удаления с сервера');
+                }
+            })
+    }
+
+    init() {
+        document.querySelector(this.container).addEventListener('click', ev => {
+            if (ev.target.classList.contains('del-item')) {
+                this.delItem(ev.target);
+            }
+        })
     }
 }
 
@@ -45,24 +116,18 @@ class ProductList {
         this.products = [];
     }
     fetchProducts() {
-        // this.products = [
-        //     { id: 1, title: 'Notebook', price: 20000, img: 'https://fakeimg.pl/250x250/282828/eae0d0/' },
-        //     { id: 2, title: 'Mouse', price: 1500, img: 'https://fakeimg.pl/250x250/282828/eae0d0/' },
-        //     { id: 3, title: 'Keyboard', price: 5000, img: 'https://fakeimg.pl/250x250/282828/eae0d0/' },
-        //     { id: 4, title: 'Gamepad', price: 4500, img: 'https://fakeimg.pl/250x250/282828/eae0d0/' }
-        // ];
-
-        getRequest(`${url}catalogData.json`)
-        .then(data => { 
-            this.products = JSON.parse(data) 
-            this.render();
-            //Информацию о суммарной стоимости выводим в консоль
-            console.log(listProduct.sum()); //Сумма всех товаров
-        })
-        .catch(text => { console.log(text) })
+        getRequest(`${API}catalogData.json`)
+            .then(data => {
+                this.products = JSON.parse(data);
+                console.log(this.products);
+                this.render();
+                //Информацию о суммарной стоимости выводим в консоль
+                console.log(listProduct.sum()); //Сумма всех товаров
+            })
+            .catch(text => { console.log(text) })
 
 
-        // getRequest(`${url}catalogData.json`, (data) => {
+        // getRequest(`${API}catalogData.json`, (data) => {
         //     console.log(data);
         //     this.products = JSON.parse(data);
         //     // console.log(this.products);
@@ -74,7 +139,7 @@ class ProductList {
     render() {
         let contentHTML = '';
         this.products.forEach(product => {
-            const item = new ProductItem(product.id, product.product_name, product.price, product.img = 'https://fakeimg.pl/250x250/282828/eae0d0/');
+            const item = new CatalogItem(product);
             contentHTML += item.getHTML();
         });
         document.querySelector('.products').insertAdjacentHTML('beforeend', contentHTML);
@@ -85,6 +150,8 @@ class ProductList {
         }, 0);
     }
 }
+
+const cart = new Cart();
 
 const listProduct = new ProductList();
 listProduct.fetchProducts();
