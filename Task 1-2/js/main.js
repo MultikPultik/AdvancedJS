cba16406e18fd373131fb70c45519561af89e8b3
AvisRara <tsiganov.da@gmail.com>
2021-03-01 06:23:31 +0400

    Выполнено последнее задание

--- Task 1-2/js/main.js
+++ Task 1-2/js/main.js
@@ -4,8 +4,22 @@
 // 'use strict';
 
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
-class ParentList {
-    constructor() { }
+class BaseCatalog {
+
+    constructor(url, container) {
+        this.catalogGoods = []; //товары в каталоге
+        this.cartGoods = [];    //товары в корзине
+        this.container = container;
+        this.url = url;
+    }
+
+    getData(url) {
+        return fetch(`${API + url}`)
+            .then(response => response.json())
+            .catch(error => {
+                console.log('Ошибка получения данных от сервера');
+            })
+    }
 }
 // переделать в ДЗ на промисы. НЕ ИСПОЛЬЗОВАТЬ fetch!!!
 let getRequest = (API) => {
@@ -39,9 +53,51 @@ class CatalogItem extends Item {
                     <h3>${this.title}</h3>
                     <img src= "${this.linkImg}" alt="someImg">
                     <p>${this.price} &#8381;</p>
-                    <button class="by-btn">Добавить в корзину</button>
+                    <button class="by-btn"
+                    data-id="${this.id}"
+                    data-title="${this.title}"
+                    data-price="${this.price}">Добавить в корзину</button>
                 </div>`;
     }
+}
+
+class Catalog extends BaseCatalog {
+    constructor(cart, container = '.products', url = 'catalogData.json') {
+        super(url, container);
+        this.cart = cart;
+        this.init();
+    }
+
+    init() {
+        this.fetchProducts();
+        this.render();
+        document.querySelector(this.container).addEventListener('click', (ev) => {
+            if (ev.target.classList.contains('by-btn')) {
+                this.cart.addProduct(ev.target);
+            }
+        });
+    }
+
+    fetchProducts() {
+        this.getData(this.url)
+            .then(data => {
+
+                console.log('Данные от сервера для каталога - ', data);
+                this.catalogGoods = data;
+                this.render();
+            });
+    }
+
+    render() {
+
+        let contentHTML = '';
+        this.catalogGoods.forEach(product => {
+            const item = new CatalogItem(product);
+            contentHTML += item.getHTML();
+        });
+        document.querySelector('.products').insertAdjacentHTML('beforeend', contentHTML);
+    }
+
 }
 
 class CartItem extends Item {
@@ -50,111 +106,136 @@ class CartItem extends Item {
         this.quantity = el.quantity;
     }
     getHTML() {
-        return `<tr class="cart-item" data-id="${this.id}>
+        return `<tr class="cart-item" data-id="${this.id}">
                     <td>
                         <img src="${this.linkImg}" alt="someImg" />
                     </td>
                     <td>${this.title}</td>
-                    <td>х ${this.quantity}</td>
+                    <td class="quantity">х ${this.quantity}</td>
                     <td>${this.price} &#8381;</td>
                     <td>
-                        <a href="#"><i class="bi bi-x-circle" data-id="${this.id}></i></a>
+                        <a href="#"><i class="bi bi-x-circle del-item" data-id="${this.id}"></i></a>
                     </td>
                 </tr>`;
     }
 }
 
-class Cart {
+class Cart extends BaseCatalog {
     constructor(container = ".cart-container", url = 'getBasket.json') {
-        // super(el)
+        super(url, container);
         this.container = container;
-        this.listCart = [];
-        this.getData(url);
         this.init();
     }
 
-    getData(url) {
-        return fetch(`${API + url}`)
-            .then(response => response.json())
-            // .then(data => {
-            //     console.log('Данные от сервера\n', data)
-            //     return data;
-            // })
-            .catch(error => {
-                console.log('Ошибка получения данных');
+    init() {
+        this.fetchProducts();
+        document.querySelector(this.container).addEventListener('click', ev => {
+            if (ev.target.classList.contains('del-item')) {
+                this.delProduct(ev.target);
+            }
+        })
+    }
+
+    render() {
+        this.cartGoods.forEach(element => {
+            let contentHTML = '';
+            const item = new CartItem(element);
+            contentHTML += item.getHTML();
+            document.querySelector(this.container).insertAdjacentHTML('beforeend', contentHTML);
+            
+            document.querySelector('.total-price').textContent = this.sum();
+            this.updateBadge();
+        });
+    }
+
+    fetchProducts() {
+        this.getData(this.url)
+            .then(data => {
+                console.log('Данные от сервера для корзины - ', data);
+                this.cartGoods = data.contents;
+                console.log('Товары в корзине - ', this.cartGoods);
+                this.render();
+            });
+    }
+
+    sum() {
+        return this.cartGoods.reduce((accum, curr) => {
+            return accum + curr.price * curr.quantity;
+        }, 0);
+    }
+
+    update(el) {
+        let prod = document.querySelector(`.cart-item[data-id="${el.id_product}"]`);
+        prod.querySelector('.quantity').textContent = `x ${el.quantity}`;
+
+        document.querySelector('.total-price').textContent = this.sum();
+        this.updateBadge();
+    }
+
+    updateBadge() {
+        let block = document.querySelector('.shopping-cart');
+        block.querySelector('.badge').textContent = this.cartGoods.length;
+        block = document.querySelector('nav');
+        block.querySelector('.badge').textContent = this.cartGoods.length;
+    }
+
+    addProduct(el) {
+        this.getData('addToBasket.json')
+            .then(data => {
+                if (data.result === 1) {
+                    let findElement = this.cartGoods.find(prod => { return prod.id_product === +el.dataset['id'] });
+                    if (findElement) {
+                        console.log('такой товар есть');
+                        findElement.quantity += 1;
+                        this.update(findElement);
+                        console.log(this.cartGoods);
+                    } else {
+                        console.log('такого товара нет');
+                        let product = {
+                            id_product: +el.dataset['id'],
+                            product_name: el.dataset['title'],
+                            price: +el.dataset['price'],
+                            quantity: 1,
+                        }
+                        const item = new CartItem(product);
+                        const contentHTML = item.getHTML();
+                        document.querySelector(this.container).insertAdjacentHTML('beforeend', contentHTML);
+                        this.cartGoods.push(product);
+                        console.log(this.cartGoods);
+                        
+                        document.querySelector('.total-price').textContent = this.sum();
+                        this.updateBadge();
+                    }
+                }
             })
     }
 
-    getHTML(el) {
-
-    }
-
-    delItem(el) {
+    delProduct(el) {
         this.getData('deleteFromBasket.json')
             .then(data => {
                 if (data.result === 1) {
                     let dataId = +el.dataset['id'];
-                    document.querySelector(`.cart-item[data-id="${dataId}"]`).remove();
-                    console.log('удален товар с id =', dataId)
+                    let findElement = this.cartGoods.find(prod => { return prod.id_product === +el.dataset['id'] });
+                    if (findElement.quantity > 1) {
+                        findElement.quantity -= 1;
+                        this.update(findElement);
+                    } else {
+                        document.querySelector(`.cart-item[data-id="${dataId}"]`).remove();
+                        let index = this.cartGoods.findIndex(el => el.id_product === dataId);
+                        this.cartGoods.splice(index, 1);
+                        console.log('удален товар с id =', dataId);
+                        console.log('теперь в корзине - ', this.cartGoods);
+                        
+                        document.querySelector('.total-price').textContent = this.sum();
+                        this.updateBadge();
+                    }
                 } else {
                     console.log('ошибка удаления с сервера');
                 }
             })
     }
 
-    init() {
-        document.querySelector(this.container).addEventListener('click', ev => {
-            if (ev.target.classList.contains('del-item')) {
-                this.delItem(ev.target);
-            }
-        })
-    }
-}
-
-class ProductList {
-    constructor() {
-        this.products = [];
-    }
-    fetchProducts() {
-        getRequest(`${API}catalogData.json`)
-            .then(data => {
-                this.products = JSON.parse(data);
-                console.log(this.products);
-                this.render();
-                //Информацию о суммарной стоимости выводим в консоль
-                console.log(listProduct.sum()); //Сумма всех товаров
-            })
-            .catch(text => { console.log(text) })
-
-
-        // getRequest(`${API}catalogData.json`, (data) => {
-        //     console.log(data);
-        //     this.products = JSON.parse(data);
-        //     // console.log(this.products);
-        //     this.render();
-        //     //Информацию о суммарной стоимости выводим в консоль
-        //     console.log(listProduct.sum()); //Сумма всех товаров
-        // });
-    }
-    render() {
-        let contentHTML = '';
-        this.products.forEach(product => {
-            const item = new CatalogItem(product);
-            contentHTML += item.getHTML();
-        });
-        document.querySelector('.products').insertAdjacentHTML('beforeend', contentHTML);
-    }
-    sum() {
-        return this.products.reduce((accum, curr) => {
-            return accum + curr.price;
-        }, 0);
-    }
 }
 
 const cart = new Cart();
-
-const listProduct = new ProductList();
-listProduct.fetchProducts();
-
-
-
+const listProduct = new Catalog(cart);
